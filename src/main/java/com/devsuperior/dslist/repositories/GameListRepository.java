a4e4f78a83b0124ee47ee787628b4ec98d7bde66
FIRMINOenzo <enzofirmino.camp@gmail.com>
2023-05-11 22:22:21 -0300

    Mover jogos por posição

--- src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -2,6 +2,11 @@ package com.devsuperior.dslist.repositories;
 
 import com.devsuperior.dslist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE belonging SET nr_position = :newPosition WHERE id_game_list = :listId AND id_game = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
